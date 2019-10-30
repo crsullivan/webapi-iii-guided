86c493d28101f86da169f9ee11ea2922997ee0ec
Connor R. Sullivan <crsullivan@protonmail.com>
2019-10-30 09:37:42 -0700

    helmet added

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+// Global Middleware
+server.use(helmet());
 server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
